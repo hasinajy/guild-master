1d407feb3adf99175926209945daa9ec9938a91f
hasinajy <jyhasina@gmail.com>
2024-05-20 21:37:53 +0300

    fix: Update path to page 'dashboard' in redirection controller

--- src/controllers/redirection/DashboardServlet.java
+++ src/controllers/redirection/DashboardServlet.java
@@ -10,7 +10,8 @@ import jakarta.servlet.ServletException;
 public class DashboardServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("pages/dashboard.jsp").forward(req, resp);
+        String path = req.getServletContext().getRealPath("/") + "WEB-INF/jsp/";
+        req.getRequestDispatcher(path + "dashboard.jsp").forward(req, resp);
     }
 
     @Override
