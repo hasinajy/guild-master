0667604c06bc2984cd157c5144e538fa677503df
hasinajy <jyhasina@gmail.com>
2024-06-02 22:02:53 +0300

    feat: Implement doGet in TransactionCUServlet class

--- src/controllers/transaction/TransactionCUServlet.java
+++ src/controllers/transaction/TransactionCUServlet.java
@@ -5,14 +5,32 @@ import java.io.IOException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import models.Item;
+import models.PlayerFull;
+import models.Rarity;
+import models.Transaction;
+import models.TransactionFull;
+import models.Type;
 import jakarta.servlet.ServletException;
 
 public class TransactionCUServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.doGet(req, resp);
+        try {
+            if (req.getParameter("mode") != null && req.getParameter("mode").equals("u")) {
+                String transactionID = req.getParameter("transaction-id");
+                TransactionFull updatedTransaction = TransactionFull.getByID(Integer.parseInt(transactionID));
+                req.setAttribute("updated-transaction", updatedTransaction);
+            }
+
+            req.setAttribute("transaction-type-list", TransactionType.getAll());
+            req.setAttribute("item-list", Item.getAll());
+            req.setAttribute("player-list", PlayerFull.getAll());
+            req.getRequestDispatcher("WEB-INF/jsp/insertion-form/transaction-form.jsp").forward(req, resp);
+        } catch (Exception err) {
+            err.printStackTrace(resp.getWriter());
+        }
     }
 
     @Override
@@ -20,5 +38,5 @@ public class TransactionCUServlet extends HttpServlet {
         // TODO Auto-generated method stub
         super.doPost(req, resp);
     }
-    
+
 }
