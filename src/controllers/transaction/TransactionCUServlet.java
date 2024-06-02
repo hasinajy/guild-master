43acaa41a795bcb2a1f11233c522cc68e8cc449a
hasinajy <jyhasina@gmail.com>
2024-06-02 22:12:34 +0300

    feat: Implement doPost in TransactionCUServlet class

--- src/controllers/transaction/TransactionCUServlet.java
+++ src/controllers/transaction/TransactionCUServlet.java
@@ -1,16 +1,24 @@
 package controllers.transaction;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 import models.Item;
 import models.PlayerFull;
 import models.Rarity;
 import models.Transaction;
 import models.TransactionFull;
 import models.Type;
+import utils.FileProcessing;
 import jakarta.servlet.ServletException;
 
 public class TransactionCUServlet extends HttpServlet {
@@ -35,8 +43,33 @@ public class TransactionCUServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.doPost(req, resp);
+        try {
+            String url = "TransactionCU";
+            String transactionDate = req.getParameter("transaction-date");
+            LocalDate localDate = LocalDate.parse(transactionDate, DateTimeFormatter.ISO_LOCAL_DATE);
+            Date sqlDate = java.sql.Date.valueOf(localDate);
+            int transactionTypeID = Integer.parseInt(req.getParameter("transaction-type-id"));
+            int itemID = Integer.parseInt(req.getParameter("item-id"));
+            int playerID = Integer.parseInt(req.getParameter("player-id"));
+            int staffID = 1;
+
+            Transaction transaction = new Transaction(0, sqlDate, transactionTypeID, itemID, playerID, staffID, "");
+
+            if (req.getParameter("mode") != null && req.getParameter("mode").equals("u")) {
+                int transactionID = Integer.parseInt(req.getParameter("transaction-id"));
+                url += "?mode=u";
+                url += "&transaction-id=" + itemID;
+
+                transaction.setTransactionID(transactionID);
+                transaction.update();
+            } else {
+                transaction.create();
+            }
+
+            resp.sendRedirect(url);
+        } catch (Exception err) {
+            err.printStackTrace(resp.getWriter());
+        }
     }
 
 }
