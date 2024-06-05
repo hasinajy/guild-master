cced9cbc3112eff849e6f9bade1d68f754d98b4c
hasinajy <jyhasina@gmail.com>
2024-06-05 23:07:58 +0300

    feat: Add PlayerSearchCriteria class

---
+++ src/models/PlayerSearchCriteria.java
@@ -0,0 +1,50 @@
+package models;
+
+public class PlayerSearchCriteria {
+    private String username;
+    private String characterName;
+    private Integer minLevel;
+    private Integer maxLevel;
+    private Integer factionId;
+
+    // Getters and setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    public Integer getMinLevel() {
+        return minLevel;
+    }
+
+    public void setMinLevel(Integer minLevel) {
+        this.minLevel = minLevel;
+    }
+
+    public Integer getMaxLevel() {
+        return maxLevel;
+    }
+
+    public void setMaxLevel(Integer maxLevel) {
+        this.maxLevel = maxLevel;
+    }
+
+    public Integer getFactionId() {
+        return factionId;
+    }
+
+    public void setFactionId(Integer factionId) {
+        this.factionId = factionId;
+    }
+}
