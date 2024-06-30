56e2345ae740987c918070d33ccf7e05e8ad1938
hasinajy <jyhasina@gmail.com>
2024-07-01 00:35:00 +0300

    feat: Overload extractExtension method of FileProcessing class with jakarta.servlet.http.Part as argument

--- src/utils/FileProcessing.java
+++ src/utils/FileProcessing.java
@@ -1,9 +1,15 @@
 package utils;
 
+import jakarta.servlet.http.Part;
+
 public class FileProcessing extends Utility {
     public static String extractExtension(String fileName) {
         int index = fileName.lastIndexOf('.');
         return index > 0 ? fileName.substring(index) : "";
+    }
+
+    public static String extractExtension(Part imgPart) {
+        return extractExtension(imgPart.getSubmittedFileName());
     }
 
     public static String generateUniqueFileName(String extension) {
