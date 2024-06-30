15c978d1851b2f533fbf7b8b63fbe530b6e053f8
hasinajy <jyhasina@gmail.com>
2024-07-01 00:10:59 +0300

    feat: Add advanced exception logging to the doGet method of the FactionCU class

--- src/controllers/handlers/faction/FactionCU.java
+++ src/controllers/handlers/faction/FactionCU.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.logging.Level;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +15,7 @@ import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.http.Part;
 
 import models.Faction;
+import utils.ExceptionHandler;
 import utils.FileProcessing;
 import utils.RequestChecker;
 
@@ -21,15 +24,22 @@ public class FactionCU extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
+            int factionId = Integer.parseInt(req.getParameter("faction-id"));
+
             if (RequestChecker.isUpdateMode(req)) {
-                String factionID = req.getParameter("faction-id");
-                Faction updatedFaction = Faction.getByID(Integer.parseInt(factionID));
-                req.setAttribute("updated-faction", updatedFaction);
+                req.setAttribute("updated-faction", Faction.getByID(factionId));
             }
 
-            req.getRequestDispatcher("FactionForm").forward(req, resp);
-        } catch (Exception err) {
-            err.printStackTrace(resp.getWriter());
+            req.getRequestDispatcher("faction-form").forward(req, resp);
+        } catch (NumberFormatException e) {
+            ExceptionHandler.handleException(new Exception("Invalid faction id value from the request parameters", null),
+                    Level.SEVERE, resp);
+        } catch (ClassNotFoundException | SQLException e) {
+            ExceptionHandler.handleException(
+                    new Exception("An error occurred while trying to connect with the database", e), Level.SEVERE,
+                    resp);
+        } catch (ServletException | IOException e) {
+            ExceptionHandler.handleException(e, Level.SEVERE, resp);
         }
     }
 
