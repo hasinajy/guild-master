4cbea871b360e78b922527c5a472a9bce1ab8701
hasinajy <jyhasina@gmail.com>
2024-07-02 10:47:27 +0300

    feat: Overload handleException method of the ExceptionHandler class with a boolean as a parameter
    - The parameter isGet indicates the origin of the exception. The value is 'true' if it's from the doGet method. The value is 'false' if it's from the doPost method.

--- src/utils/ExceptionHandler.java
+++ src/utils/ExceptionHandler.java
@@ -1,12 +1,34 @@
 package utils;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ExceptionHandler extends Utility {
+    public static void handleException(Exception e, HttpServletResponse resp, boolean isGet) {
+        String msg;
+
+        if (e instanceof NumberFormatException) {
+            msg = "Invalid Id value from the request parameters";
+        } else if (e instanceof ServletException || e instanceof IOException) {
+            if (isGet) {
+                msg = "An error occurred while forwarding resources";
+            } else {
+                msg = "An error occurred while processing the image";
+            }
+        } else if (e instanceof ClassNotFoundException || e instanceof SQLException) {
+            msg = "An error occurred while trying to update the database";
+        } else {
+            msg = "An unexpected error occurred while processing faction form data";
+        }
+
+        ExceptionHandler.handleException(new Exception(msg, e), Level.SEVERE, resp);
+    }
+
     private static final Logger logger = Logger.getLogger(ExceptionHandler.class.getName());
 
     public static void handleException(Exception e, Level level, HttpServletResponse resp) {
