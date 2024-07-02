456c173a6804a8dd76a1d64480590fab49f0a5eb
hasinajy <jyhasina@gmail.com>
2024-07-02 10:52:26 +0300

    refactor: Refactor FactionCU class
    - Use RequestChecker utility to check for the modes.
    - Use ExceptionHandler.handleException(Exception, HttpServletResponse, boolean) to handle exceptions.
    - Use imageProcessor utility to process image.

--- src/controllers/handlers/faction/FactionCU.java
+++ src/controllers/handlers/faction/FactionCU.java
@@ -1,23 +1,16 @@
 package controllers.handlers.faction;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.sql.SQLException;
-import java.util.logging.Level;
-
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
-import jakarta.servlet.http.Part;
-
 import models.Faction;
 import utils.ExceptionHandler;
-import utils.FileProcessing;
+import utils.ImageProcessor;
 import utils.RequestChecker;
+import utils.UrlUtils;
 
 @MultipartConfig
 public class FactionCU extends HttpServlet {
@@ -27,19 +20,12 @@ public class FactionCU extends HttpServlet {
             int factionId = Integer.parseInt(req.getParameter("faction-id"));
 
             if (RequestChecker.isUpdateMode(req)) {
-                req.setAttribute("updated-faction", Faction.getByID(factionId));
+                req.setAttribute("updated-faction", Faction.getById(factionId));
             }
 
             req.getRequestDispatcher("faction-form").forward(req, resp);
-        } catch (NumberFormatException e) {
-            ExceptionHandler.handleException(new Exception("Invalid faction id value from the request parameters", null),
-                    Level.SEVERE, resp);
-        } catch (ClassNotFoundException | SQLException e) {
-            ExceptionHandler.handleException(
-                    new Exception("An error occurred while trying to connect with the database", e), Level.SEVERE,
-                    resp);
-        } catch (ServletException | IOException e) {
-            ExceptionHandler.handleException(e, Level.SEVERE, resp);
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e, resp, true);
         }
     }
 
@@ -48,52 +34,22 @@ public class FactionCU extends HttpServlet {
         try {
             String url = "faction-cu";
             String name = req.getParameter("faction-name");
-            String imgPath = "faction/";
-
-            // Img processing
-            Part imgPart = req.getPart("faction-img");
-
-            if (imgPart != null && imgPart.getSize() > 0) {
-                String ogName = imgPart.getSubmittedFileName();
-                String extension = FileProcessing.extractExtension(ogName);
-                String newName = FileProcessing.generateUniqueFileName(extension);
-                imgPath += newName;
-                String savePath = getServletContext().getRealPath("/uploads/faction");
-
-                try (InputStream inputStream = imgPart.getInputStream()) {
-                    File imgFile = new File(savePath + File.separator + newName);
-                    imgFile.createNewFile();
-
-                    FileOutputStream fileOutputStream = new FileOutputStream(imgFile);
-                    byte[] bytes = new byte[1024];
-                    int read;
-
-                    while ((read = inputStream.read(bytes)) != -1) {
-                        fileOutputStream.write(bytes, 0, read);
-                    }
-
-                    fileOutputStream.close();
-                } catch (IOException e) {
-                }
-            }
+            String imgPath = ImageProcessor.processImage(this, req, "faction");
 
             Faction faction = new Faction(0, name, imgPath);
 
-            if (req.getParameter("mode") != null && req.getParameter("mode").equals("u")) {
-                int factionID = Integer.parseInt(req.getParameter("faction-id"));
+            if (RequestChecker.isUpdateMode(req)) {
+                int factionId = Integer.parseInt(req.getParameter("faction-id"));
 
-                url += "?mode=u";
-                url += "&faction-id=" + factionID;
-
-                faction.setFactionID(factionID);
-                faction.update();
+                url = UrlUtils.prepareUpdateURL(url, "faction", factionId);
+                faction.update(factionId);
             } else {
                 faction.create();
             }
 
             resp.sendRedirect(url);
-        } catch (Exception err) {
-            err.printStackTrace(resp.getWriter());
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e, resp, false);
         }
     }
 }
