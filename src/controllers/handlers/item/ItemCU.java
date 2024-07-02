1f31c5a5bf7a780f3049dd99f106cfb14fcac12a
hasinajy <jyhasina@gmail.com>
2024-07-02 15:39:06 +0300

    refactor: Refactor ItemCU class
    - Use RequestChecker utility to check for the modes.
    - Use ExceptionHandler utility to handle exceptions.
    - Add setAttributes method to set the necessary attributes used by the views.

--- src/controllers/handlers/item/ItemCU.java
+++ src/controllers/handlers/item/ItemCU.java
@@ -1,37 +1,36 @@
 package controllers.handlers.item;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.sql.SQLException;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.Part;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
 import models.Item;
 import models.Rarity;
 import models.Type;
-import utils.FileProcessing;
+import utils.ExceptionHandler;
+import utils.ImageProcessor;
+import utils.RequestChecker;
+import utils.UrlUtils;
 
 @MultipartConfig
 public class ItemCU extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            if (req.getParameter("mode") != null && req.getParameter("mode").equals("u")) {
-                String itemID = req.getParameter("item-id");
-                Item updatedItem = Item.getByID(Integer.parseInt(itemID));
-                req.setAttribute("updated-item", updatedItem);
+            int itemId = Integer.parseInt(req.getParameter("item-id"));
+
+            if (RequestChecker.isUpdateMode(req)) {
+                req.setAttribute("updated-item", Item.getByID(itemId));
             }
 
-            req.setAttribute("rarity-list", Rarity.getAll());
-            req.setAttribute("type-list", Type.getAll());
-            req.getRequestDispatcher("WEB-INF/jsp/insertion-form/item-form.jsp").forward(req, resp);
-        } catch (Exception err) {
-            err.printStackTrace(resp.getWriter());
+            this.setAttributes(req);
+            req.getRequestDispatcher("ItemForm").forward(req, resp);
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e, resp, true);
         }
     }
 
@@ -40,53 +39,29 @@ public class ItemCU extends HttpServlet {
         try {
             String url = "ItemCU";
             String name = req.getParameter("item-name");
-            int typeID = Integer.parseInt(req.getParameter("type-id"));
-            int rarityID = Integer.parseInt(req.getParameter("rarity-id"));
-            String imgPath = "item/";
+            int typeId = Integer.parseInt(req.getParameter("type-id"));
+            int rarityId = Integer.parseInt(req.getParameter("rarity-id"));
+            String imgPath = ImageProcessor.processImage(this, req, "item");
 
-            // Img processing
-            Part imgPart = req.getPart("item-img");
+            Item item = new Item(0, name, typeId, rarityId, imgPath);
 
-            if (imgPart != null && imgPart.getSize() > 0) {
-                String ogName = imgPart.getSubmittedFileName();
-                String extension = FileProcessing.extractExtension(ogName);
-                String newName = FileProcessing.generateUniqueFileName(extension);
-                imgPath += newName;
-                String savePath = getServletContext().getRealPath("/uploads/item");
-
-                try (InputStream inputStream = imgPart.getInputStream()) {
-                    File imgFile = new File(savePath + File.separator + newName);
-                    imgFile.createNewFile();
-
-                    FileOutputStream fileOutputStream = new FileOutputStream(imgFile);
-                    byte[] bytes = new byte[1024];
-                    int read;
-
-                    while ((read = inputStream.read(bytes)) != -1) {
-                        fileOutputStream.write(bytes, 0, read);
-                    }
-
-                    fileOutputStream.close();
-                } catch (IOException e) {
-                }
-            }
-
-            Item item = new Item(0, name, typeID, rarityID, imgPath);
-
-            if (req.getParameter("mode") != null && req.getParameter("mode").equals("u")) {
-                int itemID = Integer.parseInt(req.getParameter("item-id"));
-                url += "?mode=u";
-                url += "&item-id=" + itemID;
-
-                item.setItemID(itemID);
-                item.update();
+            if (RequestChecker.isUpdateMode(req)) {
+                int itemId = Integer.parseInt(req.getParameter("item-id"));
+                
+                url = UrlUtils.prepareUpdateURL(url, "item", itemId);
+                item.update(itemId);
             } else {
                 item.create();
             }
 
             resp.sendRedirect(url);
-        } catch (Exception err) {
-            err.printStackTrace(resp.getWriter());
+        } catch (Exception e) {
+            ExceptionHandler.handleException(e, resp, false);
         }
+    }
+
+    private void setAttributes(HttpServletRequest req) throws ClassNotFoundException, SQLException {
+        req.setAttribute("rarity-list", Rarity.getAll());
+        req.setAttribute("type-list", Type.getAll());
     }
 }
