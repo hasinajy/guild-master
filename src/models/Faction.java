fad351245c4ee9520d58f37da6be9dcb114adac7
hasinajy <jyhasina@gmail.com>
2024-07-02 10:36:29 +0300

    refactor: Refactor Faction class based on SonarLint
    - Rename ID to Id.
    - Use the getter methods when accessing the value of the attributes.
    - Use List as a return value instead of ArrayList.
    - Overload update method with factionId as parameter.

--- src/models/Faction.java
+++ src/models/Faction.java
@@ -5,40 +5,41 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import database.Postgres;
 
 public class Faction {
-    private int factionID;
+    private int factionId;
     private String name;
     private String imgPath;
 
-    // Constructor
+    /* ------------------------------ Constructors ------------------------------ */
     public Faction() {
     }
 
-    public Faction(int factionID) {
-        this.factionID = factionID;
-        this.name = "Default Faction";
+    public Faction(int factionId) {
+        this.setFactionId(factionId);
+        this.setName("Default Faction");
     }
 
-    public Faction(int factionID, String name, String imgPath) {
-        this.setFactionID(factionID);
+    public Faction(int factionId, String name, String imgPath) {
+        this.setFactionId(factionId);
         this.setName(name);
         this.setImgPath(imgPath);
     }
 
-    // Getters & Setters
-    public int getFactionID() {
-        return factionID;
+    /* --------------------------- Getters and setters -------------------------- */
+    public int getFactionId() {
+        return this.factionId;
     }
 
-    public void setFactionID(int factionID) {
-        this.factionID = factionID;
+    public void setFactionId(int factionId) {
+        this.factionId = factionId;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public void setName(String name) {
@@ -46,15 +47,19 @@ public class Faction {
     }
 
     public String getImgPath() {
-        return imgPath;
+        return this.imgPath;
     }
 
     public void setImgPath(String imgPath) {
         this.imgPath = imgPath;
     }
 
-    // User methods
-    public static Faction getByID(int factionID) throws ClassNotFoundException, SQLException {
+    /* ---------------------------- Database methods ---------------------------- */
+    public static void deleteById(int factionId) throws ClassNotFoundException, SQLException {
+        new Faction(factionId).delete();
+    }
+
+    public static Faction getById(int factionId) throws ClassNotFoundException, SQLException {
         Faction faction = null;
 
         Connection conn = null;
@@ -62,18 +67,18 @@ public class Faction {
         ResultSet rs = null;
 
         try {
-            String query = "SELECT * FROM faction WHERE faction_id = ?";
+            String query = "SELECT name, img_path FROM faction WHERE faction_id = ?";
 
             conn = Postgres.getInstance().getConnection();
             stmt = conn.prepareStatement(query);
-            stmt.setInt(1, factionID);
+            stmt.setInt(1, factionId);
             rs = stmt.executeQuery();
 
             while (rs.next()) {
                 String name = rs.getString("name");
                 String imgPath = rs.getString("img_path");
 
-                faction = new Faction(factionID, name, imgPath);
+                faction = new Faction(factionId, name, imgPath);
             }
         } catch (Exception e) {
             if (conn != null) {
@@ -94,30 +99,26 @@ public class Faction {
         return faction;
     }
 
-    public static void deleteByID(int factionID) throws ClassNotFoundException, SQLException {
-        new Faction(factionID).delete();
-    }
-
-    public static ArrayList<Faction> getAll() throws ClassNotFoundException, SQLException {
-        ArrayList<Faction> data = new ArrayList<>();
+    public static List<Faction> getAll() throws ClassNotFoundException, SQLException {
+        List<Faction> data = new ArrayList<>();
 
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
 
         try {
-            String query = "SELECT * FROM faction";
+            String query = "SELECT faction_id, name, img_path FROM faction";
 
             conn = Postgres.getInstance().getConnection();
             stmt = conn.prepareStatement(query);
             rs = stmt.executeQuery();
 
             while (rs.next()) {
-                int factionID = rs.getInt("faction_id");
+                int factionId = rs.getInt("faction_id");
                 String name = rs.getString("name");
                 String imgPath = rs.getString("img_path");
 
-                data.add(new Faction(factionID, name, imgPath));
+                data.add(new Faction(factionId, name, imgPath));
             }
         } catch (Exception e) {
             if (conn != null) {
@@ -136,31 +137,6 @@ public class Faction {
         }
 
         return data;
-    }
-
-    public void delete() throws ClassNotFoundException, SQLException {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-
-        try {
-            String query = "DELETE FROM faction WHERE faction_id = ?";
-
-            conn = Postgres.getInstance().getConnection();
-            stmt = conn.prepareStatement(query);
-            stmt.setInt(1, this.factionID);
-            stmt.executeUpdate();
-        } catch (Exception e) {
-            if (conn != null) {
-                conn.rollback();
-            }
-            throw e;
-        } finally {
-            if (stmt != null)
-                stmt.close();
-
-            if (conn != null)
-                conn.close();
-        }
     }
 
     public void create() throws ClassNotFoundException, SQLException {
@@ -189,6 +165,31 @@ public class Faction {
         }
     }
 
+    public void delete() throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+            String query = "DELETE FROM faction WHERE faction_id = ?";
+
+            conn = Postgres.getInstance().getConnection();
+            stmt = conn.prepareStatement(query);
+            stmt.setInt(1, this.getFactionId());
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            if (conn != null) {
+                conn.rollback();
+            }
+            throw e;
+        } finally {
+            if (stmt != null)
+                stmt.close();
+
+            if (conn != null)
+                conn.close();
+        }
+    }
+
     public void update() throws ClassNotFoundException, SQLException {
         Connection conn = null;
         PreparedStatement stmt = null;
@@ -201,14 +202,14 @@ public class Faction {
 
                 stmt = conn.prepareStatement(query);
                 stmt.setString(1, this.getName());
-                stmt.setInt(2, this.getFactionID());
+                stmt.setInt(2, this.getFactionId());
             } else {
                 String query = "UPDATE faction SET name = ?, img_path = ? WHERE faction_id = ?";
 
                 stmt = conn.prepareStatement(query);
                 stmt.setString(1, this.getName());
                 stmt.setString(2, this.getImgPath());
-                stmt.setInt(3, this.getFactionID());
+                stmt.setInt(3, this.getFactionId());
             }
 
             stmt.executeUpdate();
@@ -224,5 +225,10 @@ public class Faction {
             if (conn != null)
                 conn.close();
         }
+    }
+
+    public void update(int factionId) throws ClassNotFoundException, SQLException {
+        this.setFactionId(factionId);
+        this.update();
     }
 }
