5f19408d4c19aadf7cab15b53f943edcc006fd55
hasinajy <jyhasina@gmail.com>
2024-07-15 10:38:31 +0300

    chore: Remove unused packages

--- src/database/PostgresResources.java
+++ src/database/PostgresResources.java
@@ -6,7 +6,6 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.time.LocalDate;
